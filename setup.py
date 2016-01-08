e3872cd3e1a71571ffeccf92b4188143ab914170
Fabio <nanook@nanook.com.br>
2016-01-08 15:21:17 -0200

    Ops...

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-print find_packages(exclude=['contrib', 'docs', 'tests'])
-
 setup(
     name='json_schema',
 
